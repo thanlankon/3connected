314fd00a951dd539d5929b0702b09249f381be6d
thanlankon <xthanlankon@gmail.com>
2014-08-09 22:09:20 +0700

    Update validators

--- client/core/validator/validator/min.js
+++ client/core/validator/validator/min.js
@@ -3,7 +3,7 @@ define('core.validator.Min', function (module, require) {
   module.exports = function (attribute, value, ruleData, rules, data) {
 
     // skip checking for null or undefined
-    if (value === null || value === undefined) return true;
+    if (value === '' || value === null || value === undefined) return true;
 
     value = +value;
 
--- client/core/validator/validator/positive-integer.js
+++ client/core/validator/validator/positive-integer.js
@@ -5,7 +5,7 @@ define('core.validator.PositiveInteger', function (module, require) {
   module.exports = function (attribute, value, rule, rules, data) {
 
     // skip checking for null or undefined
-    if (value === null || value === undefined) return true;
+    if (value === '' || value === null || value === undefined) return true;
 
     return Util.Object.isInteger(value) && value > 0;
 
