ea478cd1ab7b6b6081cadefd3f4a88a3f543c337
thanlankon <xthanlankon@gmail.com>
2014-08-12 20:40:04 +0700

    Update router

--- server/router/resource/app-router.js
+++ server/router/resource/app-router.js
@@ -3,6 +3,8 @@ define('router.App', function (module, require) {
   var appRouter = require('lib.Express').Router();
   var resource = require('resource.loader.Resource');
 
+  var Configuration = require('core.config.Configuration').getConfiguration();
+
   module.exports = appRouter;
 
   var AuthenticationMiddleware = require('router.middleware.AuthenticationMiddleware');
@@ -11,7 +13,7 @@ define('router.App', function (module, require) {
   appRouter.get('/', function (req, res) {
     var authentication = req.authentication;
 
-    var Resource = resource.getResource(true);
+    var Resource = resource.getResource(Configuration.ENABLE_DEVELOPMENT);
 
     if (authentication.isAuthenticated) {
       var content = Resource.appPage({
