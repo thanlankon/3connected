2fbe6958dfa8c7c379f25ecf3740907678902450
TrongND <trong.nd@outlook.com>
2014-08-26 14:56:14 +0700

    Update attendance lock

--- server/config/attendance.js
+++ server/config/attendance.js
@@ -2,37 +2,37 @@ define('config.Attendance', function (module, require) {
 
   var AttendanceConfig = {
     SLOT_DURATION: 90,
-    LOCK_BEFORE_STARTING: false,
+    LOCK_BEFORE_STARTING: 0,
     LOCK_AFTER_STARTING: false,
     LOCK_AFTER_ENDING: 0,
 
     Slots: {
       SLOT1: {
-        START: '07:00'
+        START: '00:00'
       },
       SLOT2: {
-        START: '08:45'
+        START: '01:45'
       },
       SLOT3: {
-        START: '10:30'
+        START: '03:30'
       },
       SLOT4: {
-        START: '12:30'
+        START: '05:30'
       },
       SLOT5: {
-        START: '14:15'
+        START: '07:15'
       },
       SLOT6: {
-        START: '16:00'
+        START: '09:00'
       },
       SLOT7: {
-        START: '18:00'
+        START: '11:00'
       },
       SLOT8: {
-        START: '19:45'
+        START: '12:45'
       },
       SLOT9: {
-        START: '21:30'
+        START: '14:30'
       }
     }
   };
